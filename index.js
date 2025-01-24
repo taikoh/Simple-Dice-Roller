c66a8f5e32044ecea5df3d75fda9c03e7572568e
Taikoh <ah.joergensen90@gmail.com>
2025-01-24 11:41:13 +0100

    .

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function diceRoller() {
 	const numOfDice = document.getElementById("numOfDice").value;
 	const diceResult = document.getElementById("diceResult");
 	const diceImage = document.getElementById("diceImages");
-    const sumOfDice = document.getElementById("sumOfDice");
+	const sumOfDice = document.getElementById("sumOfDice");
 	const values = [];
 	const images = [];
-    let sum = 0;
+	let sum = 0;
 
 	for (i = 0; i < numOfDice; i++) {
 		const value = Math.floor(Math.random() * 6) + 1;
@@ -13,12 +13,11 @@ function diceRoller() {
 		images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`);
 	}
 
-    values.forEach((item) => {
-        sum += item;
-    });
-    
-    sumOfDice.innerHTML = `Total sum: ${sum}`;
-    diceResult.textContent = `Dice: ${values.join(" - ")}`;
+	values.forEach((item) => {
+		sum += item;
+	});
+
+	sumOfDice.innerHTML = `Total sum: ${sum}`;
+	diceResult.textContent = `Dice: ${values.join(" - ")}`;
 	diceImage.innerHTML = images.join("");
 }
-
